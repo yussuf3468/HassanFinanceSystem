53c17417ee05009a8cc7de75e45a91db09ad42d4
yussuf3468 <yussufh080@gmail.com>
2026-05-09 06:31:16 +0300

    fix: wire StorefrontPage to CustomerStoreNew redesign

--- src/pages/StorefrontPage.tsx
+++ src/pages/StorefrontPage.tsx
@@ -1,9 +1,9 @@
-import HorumarStorefront from "../components/HorumarStorefront";
+import CustomerStoreNew from "../components/CustomerStoreNew";
 
 interface StorefrontPageProps {
   onAdminClick: () => void;
 }
 
 export default function StorefrontPage({ onAdminClick }: StorefrontPageProps) {
-  return <HorumarStorefront onAdminClick={onAdminClick} />;
+  return <CustomerStoreNew onAdminClick={onAdminClick} />;
 }
